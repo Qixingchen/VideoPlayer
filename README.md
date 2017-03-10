71904db6319911c202fc7880263499895c948acd
qixingchen <qixingchen@lovelive.moe>
2017-03-10 13:55:40 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+VideoPlayer
+
+[![Release](https://jitpack.io/v/Qixingchen/VideoPlayer.svg?style=flat-square)](https://jitpack.io/#Qixingchen/VideoPlayer)
+[![Build Status](https://travis-ci.org/Qixingchen/VideoPlayer.svg?branch=master)](https://travis-ci.org/Qixingchen/VideoPlayer)
+[![Coverage Status](https://coveralls.io/repos/github/Qixingchen/VideoPlayer/badge.svg)](https://coveralls.io/github/Qixingchen/VideoPlayer)
+
+---
+### download
+
+ use [jitpack](https://jitpack.io/#Qixingchen/VideoPlayer)
+
+### how to use
+
+``` java
+startActivity(PlayerActivity.getStartIntent(context,videoUrl));
+```
+or see demo app or
+read javadoc in [jitpack](https://jitpack.io/com/github/Qixingchen/VideoPlayer/-SNAPSHOT/javadoc/)
---
+++ lib/src/main/java/moe/xing/videoplayer/PlayerActivity.java
@@ -0,0 +1,70 @@
+package moe.xing.videoplayer;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import com.devbrackets.android.exomedia.listener.OnErrorListener;
+import com.devbrackets.android.exomedia.listener.OnPreparedListener;
+import com.devbrackets.android.exomedia.ui.widget.EMVideoView;
+
+
+public class PlayerActivity extends Activity implements OnPreparedListener, OnErrorListener {
+    private EMVideoView emVideoView;
+    private static final String VIDEO_URL = "VIDEO_URL";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_player);
+
+        emVideoView = (EMVideoView) findViewById(R.id.video_view);
+        emVideoView.setOnPreparedListener(this);
+        emVideoView.setOnErrorListener(this);
+        emVideoView.setVideoURI(Uri.parse(getIntent().getStringExtra(VIDEO_URL)));
+    }
+
+    @Override
+    public void onPrepared() {
+        emVideoView.start();
+    }
+
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        emVideoView.pause();
+    }
+
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        emVideoView.start();
+    }
+
+    public static Intent getStartIntent(@NonNull Context context, @NonNull String url) {
+        Intent intent = new Intent(context, PlayerActivity.class);
+        intent.putExtra(VIDEO_URL, url);
+        return intent;
+    }
+
+    /**
+     * Called to indicate an error.
+     *
+     * @return True if the method handled the error, false if it didn't.
+     */
+    @Override
+    public boolean onError() {
+        Toast.makeText(this, "无法加载视频", Toast.LENGTH_LONG).show();
+        return true;
+    }
+}
