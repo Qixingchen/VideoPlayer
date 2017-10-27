b53a2b29d7248d0f601dad010d5646611dc5f6c8
qixingchen <qixingchen@lovelive.moe>
2017-10-27 14:32:43 +0800

    feat : allow use 3rd apps to play,Orientating with system

--- lib/src/main/java/moe/xing/videoplayer/PlayerActivity.java
+++ lib/src/main/java/moe/xing/videoplayer/PlayerActivity.java
@@ -6,49 +6,42 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Toast;
 
-import com.devbrackets.android.exomedia.listener.OnErrorListener;
-import com.devbrackets.android.exomedia.listener.OnPreparedListener;
-import com.devbrackets.android.exomedia.ui.widget.EMVideoView;
+import com.jarvanmo.exoplayerview.ui.ExoVideoPlaybackControlView;
+import com.jarvanmo.exoplayerview.ui.ExoVideoView;
+import com.jarvanmo.exoplayerview.ui.SimpleMediaSource;
+
+import moe.xing.baseutils.utils.IntentUtils;
+
+import static com.jarvanmo.exoplayerview.ui.ExoVideoPlaybackControlView.SENSOR_LANDSCAPE;
+import static com.jarvanmo.exoplayerview.ui.ExoVideoPlaybackControlView.SENSOR_PORTRAIT;
 
 
-public class PlayerActivity extends Activity implements OnPreparedListener, OnErrorListener {
-    private EMVideoView emVideoView;
+public class PlayerActivity extends Activity {
     private static final String VIDEO_URL = "VIDEO_URL";
+    private static final String USE_3RD_APP = "USE_3RD_APP";
+    @Nullable
+    private ExoVideoView mVideoView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.activity_player);
-
-        emVideoView = (EMVideoView) findViewById(R.id.video_view);
-        emVideoView.setOnPreparedListener(this);
-        emVideoView.setOnErrorListener(this);
-        emVideoView.setVideoURI(Uri.parse(getIntent().getStringExtra(VIDEO_URL)));
-    }
-
-    @Override
-    public void onPrepared() {
-        emVideoView.start();
-    }
-
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        emVideoView.pause();
-    }
-
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        emVideoView.start();
+    /**
+     * <div class="en">get start intent</div>
+     * <div class="zh-CN">取得启动的 Intent</div>
+     *
+     * @param context        <div class="en">intent context</div>
+     *                       <div class="zh-CN">用于生成 Intent 的 Context</div>
+     * @param url            <div class="en">video url</div>
+     *                       <div class="zh-CN">视频链接</div>
+     * @param firstUse3rdApp <div class="en">use 3rd apps first</div>
+     *                       <div class="zh-CN">首先尝试使用第三方应用</div>
+     */
+    public static Intent getStartIntent(@NonNull Context context, @NonNull String url, boolean firstUse3rdApp) {
+        Intent intent = new Intent(context, PlayerActivity.class);
+        intent.putExtra(VIDEO_URL, url);
+        intent.putExtra(USE_3RD_APP, firstUse3rdApp);
+        return intent;
     }
 
     /**
@@ -61,19 +54,66 @@ public class PlayerActivity extends Activity implements OnPreparedListener, OnEr
      *                <div class="zh-CN">视频链接</div>
      */
     public static Intent getStartIntent(@NonNull Context context, @NonNull String url) {
-        Intent intent = new Intent(context, PlayerActivity.class);
-        intent.putExtra(VIDEO_URL, url);
-        return intent;
+        return getStartIntent(context, url, false);
     }
 
-    /**
-     * Called to indicate an error.
-     *
-     * @return True if the method handled the error, false if it didn't.
-     */
     @Override
-    public boolean onError() {
-        Toast.makeText(this, "无法加载视频", Toast.LENGTH_LONG).show();
-        return true;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getIntent().getBooleanExtra(USE_3RD_APP, false)) {
+            Intent playIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(getIntent().getStringExtra(VIDEO_URL)));
+            playIntent.setDataAndType(Uri.parse(getIntent().getStringExtra(VIDEO_URL)), "video/*");
+            if (IntentUtils.startIntent(playIntent)) {
+                finish();
+                return;
+            }
+        }
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_player);
+
+        mVideoView = findViewById(R.id.video_view);
+        assert mVideoView != null;
+        mVideoView.play(new SimpleMediaSource(getIntent().getStringExtra(VIDEO_URL)));
+
+        mVideoView.setOrientationListener(new ExoVideoPlaybackControlView.OrientationListener() {
+            @Override
+            public void onOrientationChange(@ExoVideoPlaybackControlView.SensorOrientationType int orientation) {
+                if (orientation == SENSOR_PORTRAIT) {
+                    mVideoView.setPortrait(true);
+                } else if (orientation == SENSOR_LANDSCAPE) {
+                    mVideoView.setPortrait(false);
+                }
+            }
+        });
+
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mVideoView != null) {
+            mVideoView.pause();
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        if (mVideoView != null) {
+            mVideoView.resume();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mVideoView != null) {
+            mVideoView.releaseSelfPlayer();
+        }
+    }
+
+
 }
--- videoplayerdemo/src/main/java/moe/xing/videoplayerdemo/MainActivity.java
+++ videoplayerdemo/src/main/java/moe/xing/videoplayerdemo/MainActivity.java
@@ -1,29 +1,23 @@
 package moe.xing.videoplayerdemo;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import moe.xing.baseutils.Init;
 import moe.xing.videoplayer.PlayerActivity;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button playBtn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        playBtn = (Button) findViewById(R.id.play_btn);
-        playBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(PlayerActivity.getStartIntent(MainActivity.this,"http://omfeajc2u.bkt.clouddn.com/%E4%B8%AD%E7%8F%AD1%E3%80%81%E5%90%89%E5%A7%86%E7%86%8A%E5%92%8C%E4%BB%96%E7%9A%84%E6%9C%8B%E5%8F%8B%EF%BC%88%E4%BA%8C%EF%BC%89%E2%80%94%E8%81%9A%E4%BC%9A%E4%B8%8A%E7%9A%84%E6%84%8F%E5%A4%96.m4v"));
-            }
-        });
+        EditText urlText = findViewById(R.id.video_url);
+        Button playBtn = findViewById(R.id.play_btn);
+        Init.init(getApplication(), true, "1.0", "videoPlayer");
+        playBtn.setOnClickListener(view -> startActivity(PlayerActivity.getStartIntent(MainActivity.this, urlText.getText().toString(), false)));
     }
 }
